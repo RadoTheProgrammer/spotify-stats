649de7a50ffb464b5def4ef5a1a3916918bf4d0a
RadoTheProgrammer <rrrado15@gmail.com>
2025-07-27 20:19:21 +0200

    sandbox2 (trend) improvements

--- sandbox2.py
+++ sandbox2.py
@@ -6,40 +6,26 @@ obj -> date
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-with open(r"my_spotify_data\Spotify Account Data\StreamingHistory_music_0.json","rb") as f:
-    #text=f.read()
-    input_data = json.load(f)
+df = pd.read_csv("StreamingHistory.csv", parse_dates=["endTime"])
 
-data={"endTime":[],"artistName":[],"trackName":[],"timePlayed":[]}
-for track in input_data:
-    data["endTime"].append(track["endTime"])
-    data["artistName"].append(track["artistName"])
-    data["trackName"].append(track["trackName"])
-    data["timePlayed"].append(track["msPlayed"])
+artist = "Duumu"
+track = ""
+#track = "Tell Me - Ellis Remix"
 
-df = pd.DataFrame(data)
-df = df.astype({"endTime":"datetime64[s]"})#, "time
-
-
-
-
-artist = "Haywyre"
-track = "Tell Me - Ellis Remix"
-df = df[(df["artistName"] == artist) & (df["trackName"] == track)]
+freq = "1D"
+if artist:
+    if track:
+        df = df[(df["artistName"] == artist) & (df["trackName"] == track)]
+    else:
+        df = df[df["artistName"] == artist]
 #df = df[df["artistName"]=="Haywyre" and df["trackName"]=="Tell Me - Ellis Remix"]
 
 #df_counts_date = df.groupby(df.endTime.dt.date).size()
-df_groups_date = df.groupby(pd.Grouper(key='endTime', freq='1D'))
-df_counts_date = df_groups_date.size()
-df_counts_date.to_csv("df_counts_date.csv")
-
-df_groups_5d = df.groupby(pd.Grouper(key='endTime', freq='5D'))
-df_counts_5d = df_groups_5d.size()
-df_counts_5d.to_csv("df_counts_5d.csv")
-df_groups_month = df.groupby(pd.Grouper(key='endTime', freq='ME'))
-df_counts_month = df_groups_month.size()
-
-plt.plot(df_counts_date.index,df_counts_date, label="5D")
+df_groups = df.groupby(pd.Grouper(key='endTime', freq=freq))
+df_counts = df_groups.size()
+#df_counts.to_csv(f"df_counts_{freq}.csv")
+print(df_counts)
+plt.bar(df_counts.index,df_counts)
 plt.gcf().autofmt_xdate()
 plt.show()
 
