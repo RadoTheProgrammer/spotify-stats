8de361ce0641d7c1a85d6c3876c4325925735cdf
RadoTheProgrammer <rrrado15@gmail.com>
2025-08-17 10:57:50 +0200

    rrstats rolling

--- liked_songs.py
+++ liked_songs.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import settings
-
+import rrstats
 FREQ = "1D"
 first_played_liked = pd.read_csv(r"data_output\first_played_liked.csv", parse_dates=["endTime"])
 first_played = pd.read_csv(r"data_output\first_played.csv", parse_dates=["endTime"])
 
 df_fpl = first_played_liked.groupby(pd.Grouper(key='endTime', freq=FREQ))
 df_fpl = df_fpl.size()
-
+df_fpl = df_fpl.rolling(5).mean()
+#df_fpl = rrstats.StatsSeries(df_fpl).myrolling(12).trimmed_mean(1)
 df_fp = first_played.groupby(pd.Grouper(key='endTime', freq=FREQ))
 df_fp = df_fp.size()
+df_fp = df_fp.rolling(12).mean()
+#df_fp = rrstats.StatsSeries(df_fp).myrolling(12).trimmed_mean(1)
 #df_counts.to_csv(f"df_counts_{freq}.csv")
 print(df_fpl.mean())
 print(df_fp.mean())
--- sandbox2.py
+++ sandbox2.py
@@ -7,9 +7,11 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import settings
+import rrstats
+
 df = pd.read_csv(settings.FILE_DATA, parse_dates=["endTime"])
 
-artist = "Duumu"
+artist = ""
 track = ""
 #track = "Tell Me - Ellis Remix"
 
@@ -24,9 +26,19 @@ if artist:
 #df_counts_date = df.groupby(df.endTime.dt.date).size()
 df_groups = df.groupby(pd.Grouper(key='endTime', freq=freq))
 df_counts = df_groups.size()
+df_counts5 = df_counts.rolling(5).mean()
+df_counts5_trimmed = rrstats.StatsSeries(df_counts).myrolling(5).trimmed_mean(1)
+df_counts12 = df_counts.rolling(12).mean()
+df_counts12_trimmed = rrstats.StatsSeries(df_counts).myrolling(12).trimmed_mean_p(0.2)
 #df_counts.to_csv(f"df_counts_{freq}.csv")
 print(df_counts)
-plt.bar(df_counts.index,df_counts)
+
+#plt.plot(df_counts.index,df_counts,label="raw")
+#plt.plot(df_counts5.index,df_counts5,label="rolling 5")
+#plt.plot(df_counts5_trimmed.index,df_counts5_trimmed,label="rolling 5 trimmed")
+plt.bar(df_counts12.index,df_counts12,label="rolling 12")
+#plt.bar(df_counts12_trimmed.index,df_counts12_trimmed,label="rolling 12 trimmed")
+plt.legend()
 plt.gcf().autofmt_xdate()
 plt.show()
 
