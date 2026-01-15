cbd0aa432f6be4d00eb0cc0a12c3f1806b01db86
RadoTheProgrammer <rrrado15@gmail.com>
2026-01-15 21:35:47 +0100

    .

--- sandbox_minplayed.py
+++ sandbox_minplayed.py
@@ -10,7 +10,7 @@ import settings
 
 df = pd.read_csv(settings.FILE_DATA, parse_dates=["endTime"])
 
-df = df[(df["endTime"]>"2025-01-01") & (df["endTime"]<"2025-11-15")]
+#df = df[(df["endTime"]>"2025-01-01") & (df["endTime"]<"2025-11-15")]
 totalplayed = 0
 for track in df.itertuples():
     minPlayed = track.msPlayed/60000  # type: float 
@@ -23,11 +23,11 @@ for track in df.itertuples():
 print(df["msPlayed"].sum() / 60000)
 # 18573, 17869
 df_artist = df.groupby("artistName")
-df_artist_size = df_artist.size().sort_values(ascending=False)
+df_artist_size = (df_artist["msPlayed"].sum() / 60000).sort_values(ascending=False)
 df_artist_size.to_csv(settings.DIR_TRG+"/artist_play_counts_minplayed.csv")
 
 df_track = df.groupby(["artistName", "trackName"])
-df_track_size = df_track.size()
+df_track_size = (df_track["msPlayed"].sum() / 60000).sort_values(ascending=False)
 df_track_size = df_track_size.sort_values(ascending=False)
 df_track_size.to_csv(settings.DIR_TRG+"/track_play_counts_minplayed.csv")
 
